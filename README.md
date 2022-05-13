47ba730b819cb33b4c0585a138baeedd87c36f3a
Lei Mao <dukeleimao@gmail.com>
2022-05-13 09:26:32 -0700

    Update

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-Nsight-Systems-Docker-Image
+# Nsight Systems Docker Image
 
+## Introduction
 
-docker build -f nsight-systems.Dockerfile --no-cache --tag=nsight-systems:11.4 .
+This is a portable Nsight Systems Docker image which allows the user to profile executables anywhere using the Nsight Systems inside the Docker container.
 
-xhost +
-docker run -it --rm --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_ADMIN --security-opt seccomp=unconfined -v $(pwd):/mnt --network=host nsight-systems:11.4
-xhost -
+## Usages
+
+### Build Docker Image
+
+To build the Docker image, please run the following command.
+
+```bash
+$ docker build -f nsight-systems.Dockerfile --no-cache --tag=nsight-systems:11.4 .
+```
+
+### Run Docker Container
+
+To run the Docker container, please run the following command.
+
+```bash
+$ xhost +
+$ docker run -it --rm --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_ADMIN --security-opt seccomp=unconfined -v $(pwd):/mnt --network=host nsight-systems:11.4
+$ xhost -
+```
+
+### Build Examples
+
+```bash
+$ cd $(pwd):/mnt/examples
+$ nvcc async_non_pinned_memory.cu -o async_non_pinned_memory
+$ nvcc async_pinned_memory.cu -o async_pinned_memory
+```
+
+### Run Nsight Systems
+
+```bash
+$ nsys-ui
+```
---
+++ examples/async_non_pinned_memory.cu
@@ -0,0 +1,83 @@
+#include <cuda_runtime.h>
+#include <iostream>
+#include <vector>
+
+#define CHECK_CUDA_ERROR(val) check((val), #val, __FILE__, __LINE__)
+template <typename T>
+void check(T err, const char* const func, const char* const file,
+           const int line)
+{
+    if (err != cudaSuccess)
+    {
+        std::cerr << "CUDA Runtime Error at: " << file << ":" << line
+                  << std::endl;
+        std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+#define CHECK_LAST_CUDA_ERROR() checkLast(__FILE__, __LINE__)
+void checkLast(const char* const file, const int line)
+{
+    cudaError_t err{cudaGetLastError()};
+    if (err != cudaSuccess)
+    {
+        std::cerr << "CUDA Runtime Error at: " << file << ":" << line
+                  << std::endl;
+        std::cerr << cudaGetErrorString(err) << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+__global__ void kernel(float* a, int offset)
+{
+    int i = offset + threadIdx.x + blockIdx.x * blockDim.x;
+    float x = (float)i;
+    float s = sinf(x);
+    float c = cosf(x);
+    a[i] = a[i] + sqrtf(s * s + c * c);
+}
+
+int main(int argc, char** argv)
+{
+    const int blockSize = 256, numStreams = 4;
+    const int n = 4 * 1024 * blockSize * numStreams;
+    const int streamSize = n / numStreams;
+    const int streamBytes = streamSize * sizeof(float);
+    const int bytes = n * sizeof(float);
+
+    // allocate pinned host memory and device memory
+    std::vector<float> vec(n, 0);
+    float *h_a, *d_a;
+    h_a = vec.data();
+    CHECK_CUDA_ERROR(cudaMalloc((void**)&d_a, bytes));
+
+    cudaStream_t streams[numStreams];
+    for (int i = 0; i < numStreams; ++i)
+    {
+        CHECK_CUDA_ERROR(cudaStreamCreate(&streams[i]));
+    }
+
+    for (int i = 0; i < numStreams; ++i)
+    {
+        int offset = i * streamSize;
+        CHECK_CUDA_ERROR(cudaMemcpyAsync(&d_a[offset], &h_a[offset],
+                                         streamBytes, cudaMemcpyHostToDevice,
+                                         streams[i]));
+        kernel<<<streamSize / blockSize, blockSize, 0, streams[i]>>>(d_a,
+                                                                     offset);
+        CHECK_LAST_CUDA_ERROR();
+        CHECK_CUDA_ERROR(cudaMemcpyAsync(&h_a[offset], &d_a[offset],
+                                         streamBytes, cudaMemcpyDeviceToHost,
+                                         streams[i]));
+    }
+
+    for (int i = 0; i < numStreams; ++i)
+    {
+        CHECK_CUDA_ERROR(cudaStreamDestroy(streams[i]));
+    }
+
+    cudaFree(d_a);
+
+    return 0;
+}
